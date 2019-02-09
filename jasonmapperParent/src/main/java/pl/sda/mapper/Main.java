d8aadfbb51816d03875f53de7e034c9f99bd54b5
Pandragona <asiuspospolitus@gmail.com>
2019-02-09 14:47:14 +0100

    coping list to jasonMapperParent main. add code to generate file json

--- jasonmapperParent/src/main/java/pl/sda/mapper/Main.java
+++ jasonmapperParent/src/main/java/pl/sda/mapper/Main.java
@@ -1,8 +1,44 @@
 package pl.sda.mapper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.sda.Lambda.Movie;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        List<Movie> movies = Arrays.asList(
+                new Movie("Tytul",
+                        "Jan",
+                        LocalDate.of(2012,02,12),
+                        60,
+                        Arrays.asList("Jan", "Maciek")),
+                new Movie("Tytul2",
+                        "Jan",
+                        LocalDate.of(2012,02,12),
+                        20,
+                        Arrays.asList("Michal", "Maciek")),
+                new Movie("Tytul2",
+                        "Kamil",
+                        LocalDate.of(2012, 02, 12),
+                        20,
+                        Arrays.asList("Jan", "Maciej"))
+        );
+
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("movies.json");
+
+        try {
+            mapper.writeValue(file, movies);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
